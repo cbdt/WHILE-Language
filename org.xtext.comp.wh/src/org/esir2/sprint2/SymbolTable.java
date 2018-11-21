e4268783e0a42a50513adf50d0661be1045a628c
Yoann <yoann4997@hotmail.fr>
2018-11-21 17:39:07 +0100

    getFunctionInternal implementation

--- org.xtext.comp.wh/src/org/esir2/sprint2/SymbolTable.java
+++ org.xtext.comp.wh/src/org/esir2/sprint2/SymbolTable.java
@@ -7,18 +7,28 @@ public class SymbolTable {
 	
 	private Map<String, FunctionInternal> functions;
 	
+	private static int functionCounter = 0;
+	
 	public SymbolTable() {
 		functions = new HashMap<>();
 	}
 	
-	public void addFunction(String name, FunctionInternal internal) {
+	public void addFunction(String name, int nbInput, int nbOutput) throws Exception {
 		// TODO
-		throw new UnsupportedOperationException("addFunction not implemented");
+		if(!hasFunction(name)) {
+			functions.put(name, new FunctionInternal("f" + functionCounter, nbInput, nbOutput));
+			functionCounter++;
+		}
+		else {
+			throw new Exception("addFunction : La fonction " + name + " est déjà définie");
+		}
 	}
 	
-	public FunctionInternal getFunctionInternal(String name) {
-		// TODO
-		throw new UnsupportedOperationException("getFunctionInternal not implemented");
+	public FunctionInternal getFunctionInternal(String name) throws Exception {
+		if(!hasFunction(name)) {
+			throw new Exception("getFunctionInternal : La fonction " + name + " n'est pas définie");
+		}
+		return functions.get(name);
 	}
 	
 	public boolean hasFunction(String name) {
