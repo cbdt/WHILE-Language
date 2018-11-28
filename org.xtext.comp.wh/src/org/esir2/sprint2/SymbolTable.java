ce5e0a66f17f815ac111fcc7a5c0e43d29405bea
Yoann <yoann4997@hotmail.fr>
2018-11-28 14:42:25 +0100

    Ajout de la table des symboles + toString code3Addr.

--- org.xtext.comp.wh/src/org/esir2/sprint2/SymbolTable.java
+++ org.xtext.comp.wh/src/org/esir2/sprint2/SymbolTable.java
@@ -2,19 +2,22 @@ package org.esir2.sprint2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class SymbolTable {
 	
 	private Map<String, FunctionInternal> functions;
-	
 	private static int functionCounter = 0;
+	
+	private Map<String, String> symbols;
+	private static int symbolCounter = 0;
 	
 	public SymbolTable() {
 		functions = new HashMap<>();
+		symbols = new HashMap<>();
 	}
 	
 	public void addFunction(String name, int nbInput, int nbOutput) throws Exception {
-		// TODO
 		if(!hasFunction(name)) {
 			functions.put(name, new FunctionInternal("f" + functionCounter, nbInput, nbOutput));
 			functionCounter++;
@@ -33,6 +36,45 @@ public class SymbolTable {
 	
 	public boolean hasFunction(String name) {
 		return functions.containsKey(name);
+	}
+	
+	public void addSymbol(String name) throws Exception {
+		if(!hasSymbol(name)) {
+			symbols.put(name, "s" + symbolCounter);
+			symbolCounter++;
+			
+		}
+		else {
+			throw new Exception("addSymbol : Le symbol " + name + " est déjà définie");
+		}
+	}
+	
+	public boolean hasSymbol(String name) {
+		return symbols.containsKey(name);
+	}
+	
+	public String getSymbol(String name) {
+		return symbols.get(name);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder codeBuilder = new StringBuilder();
+		
+		for(Entry<String, String> e : symbols.entrySet()) {
+			codeBuilder.append(e.getKey() + " : " + e.getValue()).append("\n");
+		}
+		
+		for(Entry<String, FunctionInternal> e : functions.entrySet()) {
+			FunctionInternal fi = e.getValue();
+			codeBuilder.append("function" + fi + ":").append("\n");
+			for(Code3Addr c : fi.getCode()) {
+				codeBuilder.append(c.toString() + "\n");
+			}
+			codeBuilder.append("\n");
+		}
+		
+		return codeBuilder.toString();
 	}
 
 }
