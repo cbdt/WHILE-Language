b7c705f069a6a9cc44a5434d6773e647a0a1d302
Yoann <yoann4997@hotmail.fr>
2019-01-10 16:59:06 +0100

    Ajouts de tests

--- org.xtext.comp.wh/tests/tests/boucle.test.js
+++ org.xtext.comp.wh/tests/tests/boucle.test.js
@@ -14,8 +14,7 @@ test('simple foreach', async () => {
 });*/
 
 test('inverse list foreach', async () => {
-    let result = await execute("testForeachInverse.wh", "(cons a (cons b (cons c (cons d nil))))");
-    console.log(result[0]);
+    let result = await execute("testForeachInverse.wh", "(cons a (cons b (cons c (cons d nil))))");    
     expect(result[0]).toEqual(["(cons d (cons c (cons b (cons a nil))))"]);
 });
 
@@ -25,3 +24,33 @@ test('simple while', async () => {
     expect(result[0]).toEqual([1]);
 });
 */
+
+test('for 1', async () => {
+    let result = await execute("testFor1.wh", 12);
+    expect(result[0]).toEqual(["nil"]);
+});
+
+test('for 2', async () => {
+    let result = await execute("testFor2.wh", 12);
+    expect(result[0]).toEqual([12]);
+});
+
+test('for 3', async () => {
+    let result = await execute("testFor3.wh", 5);
+    expect(result[0]).toEqual([25]);
+});
+
+test('simple foreach 1', async () => {
+    let result = await execute("testSimpleForeach1.wh", 51);
+    expect(result[0]).toEqual([0]);
+});
+
+test('simple foreach 2', async () => {
+    let result = await execute("testSimpleForeach2.wh", "(list a b c d)");
+    expect(result[0]).toEqual(["(cons d (cons c (cons b (cons a nil))))"]);
+});
+
+test('simple foreach 3', async () => {
+    let result = await execute("testSimpleForeach3.wh", 51);
+    expect(result[0]).toEqual([0]);
+});
